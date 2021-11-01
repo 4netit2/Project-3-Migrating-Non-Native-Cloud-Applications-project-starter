8cbc73833360f4205235a857f2ac061e5f58a8b4
4netIT <21337066+4netit2@users.noreply.github.com>
2021-11-01 11:53:14 +0200

    Update config.py

--- web/config.py
+++ web/config.py
@@ -4,18 +4,18 @@ app_dir = os.path.abspath(os.path.dirname(__file__))
 
 class BaseConfig:
     DEBUG = True
-    POSTGRES_URL="techconfdb.postgres.database.azure.com"  #TODO: Update value
-    POSTGRES_USER="azuser@techconfdb" #TODO: Update value
-    POSTGRES_PW="udacity_123!"   #TODO: Update value
+    POSTGRES_URL="techconfdb-server.postgres.database.azure.com"  #TODO: Update value
+    POSTGRES_USER="tech@techconfdb-server" #TODO: Update value
+    POSTGRES_PW="pass321!"   #TODO: Update value
     POSTGRES_DB="techconfdb"   #TODO: Update value
     DB_URL = 'postgresql://{user}:{pw}@{url}/{db}'.format(user=POSTGRES_USER,pw=POSTGRES_PW,url=POSTGRES_URL,db=POSTGRES_DB)
     SQLALCHEMY_DATABASE_URI = os.getenv('SQLALCHEMY_DATABASE_URI') or DB_URL
     CONFERENCE_ID = 1
     SECRET_KEY = 'LWd2tzlprdGHCIPHTd4tp5SBFgDszm'
-    SERVICE_BUS_CONNECTION_STRING ='Endpoint=sb://servicebusudacity.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=R0M5Hok2sPjeOI+UsCs6pz/chMfdHKr7pdONK32afRI=' #TODO: Update value
+    SERVICE_BUS_CONNECTION_STRING ='Endpoint=sb://review.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=EMSl+sY1I8wYZ0EJP9ysaB2pdxY7EPXFS5rJE2LvUY0=' #TODO: Update value
     SERVICE_BUS_QUEUE_NAME ='notificationqueue'
     ADMIN_EMAIL_ADDRESS: 'info@techconf.com'
-    SENDGRID_API_KEY = '' #Configuration not required, required SendGrid Account
+    SENDGRID_API_KEY = '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' #Configuration not required, required SendGrid Account
 
 class DevelopmentConfig(BaseConfig):
     DEBUG = True
